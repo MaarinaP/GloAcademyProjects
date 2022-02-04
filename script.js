39f025e31289035eb80dce379ceb0ac322d0c414
Marina Pavlova <marinap.learning@gmail.com>
2022-02-04 10:13:36 +0000

    Write in console the types of 3 variables

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ let rollback = 99;
 let fullPrice = 5000;
 let adaptive = true;
 
-console.log("Надпись в консоле");
+// console.log("Надпись в консоле");
 
-alert("Нажми на Ок");
+// alert("Нажми на Ок");
+
+console.log("Тип данных в переменной title - " + typeof(title));
+console.log(`Тип данных в переменной fullPrice - ${typeof(fullPrice)}`);
+console.log("Тип данных в переменной adaptive -", typeof(adaptive));
