5a596665f2f2205ab694c3237e9e5a4a62a463ef
Marina Pavlova <marinap.learning@gmail.com>
2022-02-04 15:52:58 +0000

    Complete all main tasks for the lesson 2

--- script.js
+++ script.js
@@ -2,15 +2,22 @@
 
 let title = "My Learning Project";
 let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 1000;
-let rollback = 99;
-let fullPrice = 5000;
+let screenPrice = 500;
+let rollback = 15;
+let fullPrice = 1000;
 let adaptive = true;
 
 // console.log("Надпись в консоле");
-
 // alert("Нажми на Ок");
 
 console.log("Тип данных в переменной title - " + typeof(title));
 console.log(`Тип данных в переменной fullPrice - ${typeof(fullPrice)}`);
 console.log("Тип данных в переменной adaptive -", typeof(adaptive));
+
+console.log("Длина строки переменной screens равна", screens.length);
+console.log("Стоимость верстки экранов", screenPrice, "фунтов");
+console.log("Стоимость разработки сайта", fullPrice,  "фунтов");
+
+console.log(screens.toLowerCase().split(", "));
+
+console.log("Процент отката посреднику за работу =", (fullPrice * (rollback/100)), "фунтов");
