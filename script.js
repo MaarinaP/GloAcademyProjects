f754197bed6a57c9e30d2c65589b7e086243280c
Marina Pavlova <marinap.learning@gmail.com>
2022-02-06 18:07:42 +0000

    Clean code as per curator advise

--- script.js
+++ script.js
@@ -1,42 +1,13 @@
 'use strict';
 
-//lesson 1
-let title = "My Learning Project";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 500;
-let rollback = 15;
-let fullPrice = 1000;
-let adaptive = true;
-
-// console.log("Надпись в консоле");
-// alert("Нажми на Ок");
-
-// lesson 2
-console.log("\n lesson 2 ------------\n");
-console.log("Тип данных в переменной title - " + typeof(title));
-console.log(`Тип данных в переменной fullPrice - ${typeof(fullPrice)}`);
-console.log("Тип данных в переменной adaptive -", typeof(adaptive));
-
-console.log("Длина строки переменной screens равна", screens.length);
-console.log("Стоимость верстки экранов", screenPrice, "фунтов");
-console.log("Стоимость разработки сайта", fullPrice,  "фунтов");
-
-console.log(screens.toLowerCase().split(", "));
-
-console.log("Процент отката посреднику за работу =", (fullPrice * (rollback/100)), "фунтов");
-
-//lesson 3
-
-console.log("\n lesson 3 ------------\n");
-
-title = prompt("Как называется ваш проект?");
-screens = prompt("Какие типы экранов нужно разработать?");
-screenPrice = Number(prompt("Сколько будет стоить данная работа?"));
-adaptive = confirm("Нужен ли адаптив на сайте?");
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = Number(prompt("Сколько будет стоить данная работа?"));
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = 1 * (prompt("Сколько это будет стоить?"));
+let servicePrice1 = Number(prompt("Сколько это будет стоить?"));
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = ~~(prompt("Сколько это будет стоить?"));
+let servicePrice2 = Number(prompt("Сколько это будет стоить?"));
 
 /*prompt check
 console.log(title);
@@ -46,10 +17,12 @@ console.log(adaptive);
 console.log(service1, typeof servicePrice1, servicePrice1);
 console.log(service2, typeof servicePrice2, servicePrice2);*/
 
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let rollback = 15;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 console.log("Данная работа обойдется клиенту в", fullPrice, "фунтов");
 
 let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
+console.log("Процент отката посреднику за работу =", (fullPrice * (rollback/100)), "фунтов");
 console.log("С вычетом отката посреднику я получу", servicePercentPrice, "фунтов");
 
 if (fullPrice < 0 || isNaN(fullPrice)) {
